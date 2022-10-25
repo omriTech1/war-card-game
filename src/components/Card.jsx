8db8d6860766efe254a9b3a113c80645e662e815
Omri Luz <omri.l@tech.co.il>
2022-10-25 10:03:24 +0300

    fix for issue #10

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,40 +1,41 @@
 import React from "react";
 
 const Card = ({ number, type }) => {
-  let letter = null
+  let symbol = null;
   const textColor = ["heart", "diamond"].includes(type)
     ? "text-red-600"
     : "text-black";
 
   if (number === 1) {
-    letter = 'A'
-  }
-  if (number > 10) {
+    symbol = "A";
+  } else if (number > 10) {
     switch (number) {
       case 11:
-        letter = "J";
+        symbol = "J";
         break;
       case 12:
-        letter = "Q";
+        symbol = "Q";
         break;
       case 13:
-        letter = "K";
+        symbol = "K";
         break;
-    }
+    } 
+  } else {
+    symbol = number
   }
 
   return (
-    <div className="h-[500px] p-3 bg-white rounded-lg outline outline-1 w-96 flex flex-col justify-between">
-      <div className={`${textColor} text-3xl`}>{letter || number}</div>
-      <div className="text-4xl flex-1 flex justify-center items-center gap-x-3">
+    <div className="flex h-[500px] w-96 flex-col justify-between rounded-lg bg-white p-3 outline outline-1">
+      <div className={`${textColor} text-3xl`}>{symbol}</div>
+      <div className="flex flex-1 items-center justify-center gap-x-3 text-4xl">
         {/* {'❤️'.repeat(number)} */}
         <div className="flex flex-wrap">
-        {Array.from(Array(number), (x, index) => {
-          return <div key={index} className={`${type}`}></div>
-        })}
-        </div> 
+          {Array.from(Array(number), (x, index) => {
+            return <div key={index} className={`${type}`}></div>;
+          })}
+        </div>
       </div>
-      <div className={`${textColor} text-3xl self-end`}>{letter || number}</div>
+      <div className={`${textColor} self-end text-3xl`}>{symbol}</div>
     </div>
   );
 };
